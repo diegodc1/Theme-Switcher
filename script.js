5a6393233262dc60832b7019d782f7adf09dbbe7
Diego Alves <90157531+diegodc1@users.noreply.github.com>
2023-01-03 14:23:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const button = document.getElementById("button");
-
-const changeColor = document.getElementById("change-color2");
-const circle = document.getElementById("circle-transition");
-const title = document.getElementById("title");
-const author = document.getElementById("author");
-const darkModeMessage = document.getElementById("dark-mode");
-const lightModeMessage = document.getElementById("light-mode");
+const changeColor = document.getElementById('change-color')
+const button = document.getElementById('button')
+const circle = document.getElementById('circle-transition')
+const title = document.getElementById('title')
+const modeSelectMsg = document.querySelector('.mode-select')
+const author = document.getElementById('author')
 
 button.onclick = function () {
-  if (changeColor.classList.contains("dark")) {
-    changeColor.classList.remove("dark");
-    circle.classList.remove("dark");
-    title.classList.remove("dark");
-    author.classList.remove("dark");
-    lightModeMessage.classList.remove("invisible");
-    darkModeMessage.classList.add("invisible");
+  if (changeColor.classList.contains('dark')) {
+    changeColor.classList.remove('dark')
+    circle.classList.remove('dark')
+    title.classList.remove('dark')
+    author.classList.remove('dark')
+
+    modeSelectMsg.textContent = 'Light Mode'
   } else {
-    changeColor.classList.add("dark");
-    circle.classList.add("dark");
-    title.classList.add("dark");
-    author.classList.add("dark");
-    lightModeMessage.classList.add("invisible");
-    darkModeMessage.classList.remove("invisible");
+    changeColor.classList.add('dark')
+    circle.classList.add('dark')
+    title.classList.add('dark')
+    author.classList.add('dark')
+
+    modeSelectMsg.textContent = 'Dark Mode'
   }
-};
+}
